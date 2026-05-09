fcc5ed75ba3d5c35393b2fbe536d81416286fc60
NikolaTomic00 <tomicnikolaftn@gmail.com>
2026-05-09 19:46:01 +0200

    testing deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,16 +4,26 @@ import dotenv from "dotenv";
 
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
-
+app.use(
+  cors({
+    origin: "https://pawmeet-1.onrender.com",
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(cookieParser());
 
 app.get("/api/health", (req, res) => {
   res.json({ ok: true });
+});
+
+app.get("/api/test", (req, res) => {
+  res.json({ message: "backend radi ✔" });
 });
 
 app.listen(ENV.PORT, () => {
