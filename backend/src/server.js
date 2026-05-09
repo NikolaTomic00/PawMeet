7ca8dee192fae27a2399b25a5851be1b16eade26
NikolaTomic00 <tomicnikolaftn@gmail.com>
2026-05-09 18:11:51 +0200

    ready for deployment

---
+++ backend/src/server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+
+//import cors from "cors";
+
+import { ENV } from "./lib/env.js";
+import { connectDB } from "./lib/db.js";
+
+dotenv.config();
+
+const app = express();
+const __dirname = path.resolve();
+
+app.use(express.json());
+app.use(cookieParser());
+
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+app.listen(ENV.PORT, () => {
+  console.log(`Server running on port ${ENV.PORT}`);
+
+  connectDB();
+});
