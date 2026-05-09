78e5436651242d121cae2bbdb66339b4b733c78e
NikolaTomic00 <tomicnikolaftn@gmail.com>
2026-05-09 22:05:29 +0200

    added cloudanary to backend

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -1,9 +1,11 @@
 import "dotenv/config";
 
-
-
 export const ENV = {
   PORT: process.env.PORT,
   MONGO_URI: process.env.MONGO_URI,
   NODE_ENV: process.env.NODE_ENV,
+  JWT_SECRET: process.env.JWT_SECRET,
+  CLAUDINARY_CLOUD_NAME: process.env.CLAUDINARY_CLOUD_NAME,
+  CLAUDINARY_API_KEY: process.env.CLAUDINARY_API_KEY,
+  CLAUDINARY_API_SECRET: process.env.CLAUDINARY_API_SECRET,
 };
