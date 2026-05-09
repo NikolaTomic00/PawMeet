5b9bd73ecbdc94b040753b01bed534d0ace04908
NikolaTomic00 <tomicnikolaftn@gmail.com>
2026-05-09 18:58:09 +0200

    imported path from path

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,8 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
-
-//import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
@@ -10,7 +10,9 @@ import { connectDB } from "./lib/db.js";
 dotenv.config();
 
 const app = express();
-const __dirname = path.resolve();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(express.json());
 app.use(cookieParser());
@@ -19,12 +21,11 @@ if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (_, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
   });
 }
 
 app.listen(ENV.PORT, () => {
   console.log(`Server running on port ${ENV.PORT}`);
-
   connectDB();
 });
